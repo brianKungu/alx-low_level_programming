56686789a03f2e391198721605a7895fdde27b63
Brian Kungu <bkungu07@gmail.com>
2022-03-19 01:14:45 +0300

    isdigit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isupper - Function that checks for uppercase character
- * @c: Number tu be checked
+ * @c: Number to be checked
  * Return: 1 if c is uppercase and 0 otherwise
  */
 int _isupper(int c)
