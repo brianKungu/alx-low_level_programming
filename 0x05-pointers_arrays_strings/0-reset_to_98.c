12bc96ecf0a4cd0907d79ad8e2c9e70a576c905c
Brian Kungu <bkungu07@gmail.com>
2022-03-21 16:26:59 +0300

    098A

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
  * reset_to_98 - Function that takes a pointer and updates the value
  * @n: Value that update
  */
- void reset_to_98(int *n)
- {
-   *n = 98;
- }
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
