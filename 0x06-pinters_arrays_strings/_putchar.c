f670a52aa97f2133809573b2f53bd31a06227118
brianKungu <bkungu07@gmail.com>
2022-03-24 16:31:46 +0300

    concat

--- 0x06-pinters_arrays_strings/_putchar.c
+++ 0x06-pinters_arrays_strings/_putchar.c
@@ -1,22 +1,13 @@
 #include "main.h"
-#include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - check the code
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: Always 0.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
-
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strcat(s1, s2);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr);
-    return (0);
+	return (write(1, &c, 1));
 }
