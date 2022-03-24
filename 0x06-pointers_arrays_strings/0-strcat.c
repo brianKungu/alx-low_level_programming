465273f643962d15f40c855f685f4470aa4c0e88
brianKungu <bkungu07@gmail.com>
2022-03-24 17:12:35 +0300

    concat3

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,12 +10,14 @@ char *_strcat(char *dest, char *src)
 	int i, j;
 
 	i = 0;
+
 	while (dest[i] != '\0') /*WHILE count character */
 	{
 		i++;
 	}
 
 	j = 0;
+	
 	while (src[j] != '\0') /*WHILE concatenate dest with src*/
 	{
 		dest[i] = src[j];
