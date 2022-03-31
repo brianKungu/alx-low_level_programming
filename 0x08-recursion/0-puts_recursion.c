b4f8e95f0bbca2b7464083b0292cf1b7ae8ba528
brianKungu <bkungu07@gmail.com>
2022-03-31 09:11:13 +0300

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+<<<<<<< HEAD
 
 /**
  * main - check the code
@@ -10,3 +11,16 @@ int main(void)
     _puts_recursion("Puts with recursion");
     return (0);
 }
+=======
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function that prints a string
+ * @s: string to be printed
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
+>>>>>>> 175ac66bf02eb6d10a4a8f712895941f21b3c5a6
--- test.js
+++ test.js
@@ -1,16 +1,49 @@
-function print(a){
+// function print(a){
     
-    if (a < 0) {
-        return 0;
+//     if (a < 0) {
+//         return 0;
+//     }
+//     console.log(a + (a-1));
+//     a --;
+//     return a;
+// }
+
+// function printResult(){
+//     print(4);
+//     return 0;
+// }
+
+// printResult();
+
+const reverseString = (str) => {
+    result=str.split('').reverse().join('')
+    console.log(result);
+}
+
+const displayResuly = () => {
+    reverseString("Geeks for Geeks");
+}
+
+displayResuly();
+
+// defining an object
+
+// object -instance of a class 
+
+let person = {
+    firstName : "Brian",
+    lastName : "Kungu",
+
+    // method - property of an object whose value is a function
+    getFunction : function(){
+        return (`My name is ${person.firstname} ${person.lastName}`);
+    },
+
+    // object within an object
+    contact : {
+        mobile : "0759391973",
+        email : "bkungu07@gmail.com",
     }
-    console.log(a + (a-1));
-    a --;
-    return a;
 }
 
-function printResult(){
-    print(4);
-    return 0;
-}
-
-printResult();
+console.log(person.getFunction());
