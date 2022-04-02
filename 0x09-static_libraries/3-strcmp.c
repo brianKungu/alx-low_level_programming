0e70d5562b13037bb2a3a58f0616b1db2ed82f95
brianKungu <bkungu07@gmail.com>
2022-04-03 00:27:56 +0300

    aa

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,4 @@
+#include "main.h"
+int _strcmp(char *s1, char *s2)
+{}
+/Users/caleb/Downloads/alx-low_level_programming-master/0x09-static_libraries/3-strcmp.c
--- test.js
+++ test.js
@@ -26,24 +26,64 @@ const displayResuly = () => {
 
 displayResuly();
 
-// defining an object
-
 // object -instance of a class 
+// objects can be created in two ways
 
+// 1. Object Literal
+
+// defining an object
 let person = {
     firstName : "Brian",
     lastName : "Kungu",
 
     // method - property of an object whose value is a function
     getFunction : function(){
-        return (`My name is ${person.firstname} ${person.lastName}`);
+        return (`My name is ${person.firstName} ${person.lastName}`);
     },
 
     // object within an object
     contact : {
         mobile : "0759391973",
         email : "bkungu07@gmail.com",
+    },
+
+    // display method
+    displayResult : () => {
+        return(`My name is ${person.firstName} ${person.lastName}. \nPhone number:${person.contact.mobile}, \nEmail Address: ${person.contact.email}`)
     }
 }
 
-console.log(person.getFunction());
+console.log(person.displayResult());
+// console.log(person.contact.mobile)
+
+// 2. object constructor
+function laptop(name,core,storage){
+    this.name = name;
+    this.core = core;
+    this.storage = storage;
+    let overall = this.name + this.core + this.storage;
+    return overall;
+}
+
+// creating new instances of laptop object
+let laptop1 = new laptop("HP","i3","500GB HDD")
+console.log(`${laptop1.name} ${laptop1.core} ${laptop1.storage}`)
+
+
+
+
+// CLASSES
+class P{
+    constructor(fName, lName){
+    this.fName = fName;
+    this.lName = lName;   
+}
+    getFullNames(){
+        return this.fName + this.lName;
+    }
+}
+
+// creating object of the P class
+let individual1 = new P ("Andrew", "Nderitu");
+let individual2 = new P ("Marcus", "Mbogo");
+console.log(individual1.getFullNames())
